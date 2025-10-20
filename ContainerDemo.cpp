3565657ad6716e9a73f0c6779b456073c0390f57
joelise <jo.soyke@gmail.com>
2025-10-20 21:05:41 +1100

    Complete ListDemo

--- ContainerDemo.cpp
+++ ContainerDemo.cpp
@@ -12,56 +12,72 @@ void ContainerDemo::ArrayDemo()
 
 void ContainerDemo::ListDemo()
 {
-	std::cout << "What are the months of the year?" << std::endl;
-	std::cout << "Press Enter to continue...";
+	std::cout << "Press Enter to start... \n";
 	std::cin.get();
-	std::cout << std::endl;
 
-	std::list<std::string> months = { "Feb, ", "Mar, ", "Apr, ", "May, ", "Jun, ", "Jun, ", "Aug, ", "Sep, ", "Oct, ", "Nov, "};
+	std::cout << "Starting\n.\n.\n.\n\n";
+
+	std::cout << "What are the months of the year?... \n\n";
+	std::cin.get();
+	
+	// Creates the list
+	std::list<std::string> months = { "February, ", "March, ", "April, ", "May, ", "June, ", "June, ", "August, ", "September, ", "October, ", "November, "};
+
 	for (std::string mon : months) 
 	{
 		std::cout << mon;
 	}
-	std::cout << std::endl;
 
-	std::cout << std::endl;
-	std::cout << "Hmmm that's not quite right..." << std::endl;
-	std::cout << std::endl;
+	std::cout << std::endl << "Hmmm that's not quite right...\n\n";
 	std::cin.get();
 
-	months.push_front("Jan, ");
+	// Adds "January to the front of the list
+	months.push_front("January, ");
+
 	for (std::string mon : months)
 	{
 		std::cout << mon;
 	}
 	std::cout << std::endl;
+	
+	std::cout << "January was added but something is still missing...\n\n";
+	std::cin.get();
 
-	months.push_back("Dec");
+	// Adds "December to the end of the list
+	months.push_back("December. \n");
+
 	for (std::string mon : months)
 	{
 		std::cout << mon;
 	}
-	std::cout << std::endl;
 
-	std::list<std::string>::iterator it = months.begin();
-	std::advance(it, 7);
-	months.insert(it, "Jul, ");
+	std::cout << "Okay we have December, wait where is July? ...\n\n";
+	std::cin.get();
+
+	// Adds "July" into its correct place in the list
+	std::list<std::string>::iterator it = months.begin();	// Creates an iterator at the start of the list	
+	std::advance(it, 7);									// Moves it to the 7th item in the list
+	months.insert(it, "July, ");							// Inserts "July" in front of the 7th item (in between "June" and "August")
+
 	for (std::string mon : months)
 	{
 		std::cout << mon;
 	}
-	std::cout << std::endl;
 
-	it = months.begin();
-	std::advance(it, 6);
-	months.erase(it);
+	std::cout << "Why are there 13 months? Oh! there are two Junes...\n\n";
+	std::cin.get();
+
+	// Removes "June" from the list
+	it = months.begin();				// Moves the iterator back to the start of the list
+	std::advance(it, 6);				// Moves it to the 6th item ("June")
+	months.erase(it);					// Erases the 6th item
+
 	for (std::string mon : months)
 	{
 		std::cout << mon;
 	}
-	std::cout << std::endl;
 
-
+	std::cout << "Now it's perfect :) \n";
 }
 
 void ContainerDemo::MapDemo()
